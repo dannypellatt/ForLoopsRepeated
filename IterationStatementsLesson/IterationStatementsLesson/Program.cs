74c9929736d740a141dd58af0b5acedad3f75e7c
dannypellatt <danielpellatt92@gmail.com>
2022-07-08 14:13:32 -0400

    initial commit

---
+++ IterationStatementsLesson/IterationStatementsLesson/Program.cs
@@ -0,0 +1,99 @@
+﻿#region Exercise 1
+
+// writing a for loop 15 times to practice
+
+
+#region put here after each time so you don't cheat
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+
+    Console.WriteLine($"{i}");
+}
+
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+#endregion
+
+
+// keyword
+// initializer
+// conditional
+// increment
+// body/scope
+
+// Write below here each time:
+
+#endregion
