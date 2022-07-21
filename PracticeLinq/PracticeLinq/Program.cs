89f94b25b87c994030b39e4851c361cbb1bd1461
dannypellatt <danielpellatt92@gmail.com>
2022-07-21 10:29:28 -0400

    shrunk down using linq

--- PracticeLinq/PracticeLinq/Program.cs
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -1,5 +1,17 @@
-﻿var myFavoriteGames = new List<string>() { "Return OF The Obra Dinn", "The Outer Wilds", "The Witcher 3: Wild Hunt", "Super Mario Odyssey", "Fallout New Vegas", "What Remains Of Edith Finch", "Red Dead Redemption 2", "Spider-Man", "Prince Of Persia: The Sands Of Time" };
+﻿var myFavoriteGames = new List<string>() {
+        "Return Of The Obra Dinn",
+        "The Outer Wilds",
+        "The Witcher 3: Wild Hunt",
+        "Super Mario Odyssey",
+        "Fallout New Vegas",
+        "What Remains Of Edith Finch",
+        "Red Dead Redemption 2",
+        "Spider-Man",
+        "Prince Of Persia: The Sands Of Time" };
 
+
+Console.WriteLine("1st try: using a foreach loop:");
+Console.WriteLine("");
 
 var LengthOfGameName = myFavoriteGames.OrderByDescending(x => x.Length).ToList();
 
@@ -7,3 +19,12 @@ foreach (var game in LengthOfGameName)
 {
     Console.WriteLine(game);
 }
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("2nd try: using just LINQ:");
+Console.WriteLine("");
+
+myFavoriteGames.OrderByDescending(game => game.Length)
+               .ToList()
+               .ForEach(game => Console.WriteLine(game));
