354afa20a76ea563b7cad66af700df969bb4df19
dannypellatt <danielpellatt92@gmail.com>
2022-07-21 09:57:52 -0400

    initial commit

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,9 @@
+﻿var myFavoriteGames = new List<string>() { "Return OF The Obra Dinn", "The Outer Wilds", "The Witcher 3: Wild Hunt", "Super Mario Odyssey", "Fallout New Vegas", "What Remains Of Edith Finch", "Red Dead Redemption 2", "Spider-Man", "Prince Of Persia: The Sands Of Time" };
+
+
+var LengthOfGameName = myFavoriteGames.OrderByDescending(x => x.Length).ToList();
+
+foreach (var game in LengthOfGameName)
+{
+    Console.WriteLine(game);
+}
